935516f460c02cc6fae9b5fe1d6bb40606f96ac8
Elena Liubimova <elena.liubimova3@gmail.com>
2023-01-01 13:06:06 +0300

    feat: добавить ключи для рендера списков

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,49 +2,20 @@ import React from 'react';
 
 function Categories() {
   const [activeCategory, setActiveCategory] = React.useState(0);
-  const onclickCategory = (index) => {
-    setActiveCategory(index);
-  };
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   return (
     <div className="categories">
       <ul>
-        <li
-          className={activeCategory === 0 ? 'active' : ''}
-          onClick={() => onclickCategory(0)}
-        >
-          Все
-        </li>
-        <li
-          className={activeCategory === 1 ? 'active' : ''}
-          onClick={() => onclickCategory(1)}
-        >
-          Мясные
-        </li>
-        <li
-          className={activeCategory === 2 ? 'active' : ''}
-          onClick={() => onclickCategory(2)}
-        >
-          Вегетарианская
-        </li>
-        <li
-          className={activeCategory === 3 ? 'active' : ''}
-          onClick={() => onclickCategory(3)}
-        >
-          Гриль
-        </li>
-        <li
-          className={activeCategory === 4 ? 'active' : ''}
-          onClick={() => onclickCategory(4)}
-        >
-          Острые
-        </li>
-        <li
-          className={activeCategory === 5 ? 'active' : ''}
-          onClick={() => onclickCategory(5)}
-        >
-          Закрытые
-        </li>
+        {categories.map((value, index) => (
+          <li
+            key={index}
+            className={activeCategory === index ? 'active' : ''}
+            onClick={() => setActiveCategory(index)}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
